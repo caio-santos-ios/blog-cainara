68d905c45800add704c002cce829f3b23eca2321
caiosantos.dev@outlook.com <seu@email.com>
2023-12-26 17:41:26 -0300

    add header

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,14 +1,18 @@
+import { Header } from "@/components/Header";
 import { ListPost } from "@/components/ListPost";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
   return (
-   <main className="body">
-      <ToastContainer />
-      <section className="section">
-        <ListPost />
-      </section>
-   </main>
+    <>
+    <Header />
+    <main className="body">
+        <ToastContainer />
+        <section className="section">
+          <ListPost />
+        </section>
+    </main>
+    </>
   )
 }
---
+++ front/src/components/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+
+    return(
+        <header className="h-20 px-4 flex justify-center items-center bg-blue-500 text-white">
+            <h1 className="font-semibold text-xl">Nara Blog</h1>
+        </header>
+    )
+}
