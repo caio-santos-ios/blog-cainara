64556e734b2a29226a934e54f5ede05c15e51b1e
caiosantos.dev@outlook.com <seu@email.com>
2023-12-26 17:28:20 -0300

    finalizando

--- front/src/components/FormCreatePost.tsx
+++ front/src/components/FormCreatePost.tsx
@@ -5,17 +5,19 @@ import { Tpost } from "@/@types/post"
 import { useForm } from "react-hook-form"
 import { api } from "@/services/api"
 import { getCookie } from "cookies-next"
+import { toast } from "react-toastify"
 import { useState } from "react"
 
 export const FormCreatePost = () => {
-    const [imagem, setImagem] = useState<any>(null)
+    const [loading, setLoading] = useState(false)
 
     const auth: string | undefined = getCookie("auth")
     const authDecoded = JSON.parse(auth!)
     
-    const { register, handleSubmit } = useForm<any>()
+    const { register, handleSubmit, reset } = useForm<any>()
 
     const createPost = async (data: Tpost) => {
+        setLoading(true)
         const formData = new FormData();
         formData.append('title', data.title);
         formData.append('description', data.description);
@@ -23,29 +25,34 @@ export const FormCreatePost = () => {
         formData.append("photos", data.photos[0])
         
         try {
-            const res = await api.post("/posts", formData, { headers: { Authorization: `Bearer ${authDecoded.token}` } })
-            console.log(res)
-        } catch (error) {console.log(error)}
-
-        console.log(formData)
-        console.log(data)
+            await api.post("/posts", formData, { headers: { Authorization: `Bearer ${authDecoded.token}` } })
+            reset()
+            setLoading(false)
+        } catch (error) { setLoading(true)}
     }
     
     return(
         <form className="formReset w-[40rem]" onSubmit={handleSubmit(createPost)}>
             <label htmlFor="title" className="labelReset border-2 border-slate-300 rounded-md">
-                    <input {...register("title")} className="inputReset w-full p-2 rounded-md" placeholder="titulo" type="text" />
+                    <input required {...register("title")} className="inputReset w-full p-2 rounded-md" placeholder="titulo" type="text" />
             </label>
             <label htmlFor="description" className="labelReset border-2 border-slate-300 rounded-md">
-                    <textarea placeholder="Ecreva aqui a descrição do seu poste..." {...register("description")} className="inputReset w-full p-2 rounded-md" cols={10} rows={10}></textarea>
+                    <textarea required placeholder="Ecreva aqui a descrição do seu poste..." {...register("description")} className="inputReset w-full p-2 rounded-md" cols={10} rows={10}></textarea>
             </label>
             <label htmlFor="coverPhoto" className="labelReset border-2 border-slate-300 rounded-md">
-                    <input {...register("coverPhoto")} className="inputReset w-full p-2 rounded-md" placeholder="descrição" type="file" />
+                    <input required {...register("coverPhoto")} className="inputReset w-full p-2 rounded-md" placeholder="descrição" type="file" />
             </label>
             <label htmlFor="photos" className="labelReset border-2 border-slate-300 rounded-md">
-                    <input {...register("photos")} className="inputReset w-full p-2 rounded-md" placeholder="descrição" type="file" />
+                    <input required {...register("photos")} className="inputReset w-full p-2 rounded-md" placeholder="descrição" type="file" />
             </label>
-            <button className="btnSubmit bg-blue-500 text-white" type="submit">Criar</button>
+            <button className="btnSubmit bg-blue-500 text-white flex justify-center items-center h-12 px-4" type="submit">
+                {
+                    loading ?
+                    <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-white border-solid"></div>
+                    :
+                <div>Criar</div>
+                }
+            </button>
         </form>
     )
 }
